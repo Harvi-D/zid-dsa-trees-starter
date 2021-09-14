341ba99fbbd87512483da341527ac2900200f449
Dylan Harville <dr.dyloh@gmail.com>
2021-09-13 22:37:59 -0600

    final BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,36 +1,33 @@
 class BinarySearchTree {
-  //if key is null, object represents aan empty tree;
-  //null parent pointer means you are dealing with a root node;
   constructor(key = null, value = null, parent = null) {
     this.key = key;
     this.value = value;
     this.parent = parent;
-    //pointers to child nodes initialized as null;
     this.left = null;
     this.right = null;
   }
+
   insert(key, value) {
     // If the tree is empty, then this key being inserted 
     //is the root node of the tree.
     if (this.key == null) {
       this.key = key;
       this.value = value;
-
     /* If the tree already exists, then start at the root,
     and compare it to the key that you want to insert.
     If the new key is less than the node's key,
-    then the new node needs to live in the left-hand branch. */
+    then the new node needs to live in the left-hand branch. */  
     } else if (key < this.key) {
       /* If the existing node does not have a left child,
       meaning that the `left` pointer is empty,
       then you can just instantiate and insert the new node
       as the left child of that node, passing `this` as the parent. */
       if (this.left == null) {
-        this.left == new BinarySearchTree(key, value, this);
-      /* If the node has an existing left child,
+        this.left = new BinarySearchTree(key, value, this);
+        /* If the node has an existing left child,
       then you recursively call the `insert()` method
       so that the node is added further down the tree. */
-      }else {
+      } else {
         this.left.insert(key, value);
       }
     /* Similarly, if the new key is greater than the node's key,
@@ -43,31 +40,30 @@ class BinarySearchTree {
       }
     }
   }
+
   find(key) {
     // if the item is found at the root, then return that value;
     if (this.key == key) {
       return this.value;
-    }
     /*if the item that you are looking for is less than the root,
     then follow the left child. If there is and existing left child,
     then recursively check its left and/or right child 
     until you find the item.*/
-    else if (key < this.key && this.left) {
+    } else if (key < this.key && this.left) {
       return this.left.find(key);
-    }
     /*if the item that you are looking for is greater than the root,
     then follow the right child.
     If there is an existing right child,
     then recursivly check its left and/or right child
     until you find the item*/
-    else if (key > this.key && this.right) {
+    } else if (key > this.key && this.right) {
       return this.right.find(key);
-    }
     //you have searched the tree, and the item isn't in the tree.
-    else {
-      throw new Error('Key Not Found');
+    } else {
+      throw new Error("Key Not Found");
     }
   }
+
   remove(key) {
     if (this.key == key) {
       if (this.left && this.right) {
@@ -75,54 +71,47 @@ class BinarySearchTree {
         this.key = successor.key;
         this.value = successor.value;
         successor.remove(successor.key);
-      }
       /* If the node only has a left child,
       then you replace the node with its left child. */
-      else if (this.left) {
-          this._replaceWith(this.left);
-      }
+      } else if (this.left) {
+        this._replaceWith(this.left);
       /* And similarly, if the node only has a right child,
       then you replace it with its right child. */
-      else if (this.right) {
+      } else if (this.right) {
         this._replaceWith(this.right);
-      }
       /* If the node has no children, then
       simply remove it and any references to it
       by calling `this._replaceWith(null)`. */
-      else {
+      } else {
         this._replaceWith(null);
       }
-    }
-    else if (key < this.key && this.left) {
+    } else if (key < this.key && this.left) {
       this.left.remove(key);
-    }
-    else if (key > this.key && this.right) {
+    } else if (key > this.key && this.right) {
       this.right.remove(key);
-    }
-    else {
-      throw new Error('Key Not Found');
+    } else {
+      throw new Error("Key Not Found");
     }
   }
+
   _replaceWith(node) {
     if (this.parent) {
       if (this == this.parent.left) {
         this.parent.left = node;
-      }
-      else if (this == this.parent.right) {
+      } else if (this == this.parent.right) {
         this.parent.right = node;
       }
+
       if (node) {
         node.parent = this.parent;
       }
-    }
-    else {
+    } else {
       if (node) {
         this.key = node.key;
         this.value = node.value;
         this.left = node.left;
         this.right = node.right;
-      }
-      else {
+      } else {
         this.key = null;
         this.value = null;
         this.left = null;
@@ -130,6 +119,7 @@ class BinarySearchTree {
       }
     }
   }
+
   _findMin() {
     if (!this.left) {
       return this;
@@ -137,3 +127,5 @@ class BinarySearchTree {
     return this.left._findMin();
   }
 }
+
+module.exports = BinarySearchTree;
