f72f8c0b7a8a3f58cb0b4699df135cea72c1441b
Dylan Harville <dr.dyloh@gmail.com>
2021-09-13 20:38:42 -0600

    find method added to BST

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -21,17 +21,51 @@ class BinarySearchTree {
     If the new key is less than the node's key,
     then the new node needs to live in the left-hand branch. */
     } else if (key < this.key) {
+      /* If the existing node does not have a left child,
+      meaning that the `left` pointer is empty,
+      then you can just instantiate and insert the new node
+      as the left child of that node, passing `this` as the parent. */
       if (this.left == null) {
         this.left == new BinarySearchTree(key, value, this);
+      /* If the node has an existing left child,
+      then you recursively call the `insert()` method
+      so that the node is added further down the tree. */
       }else {
         this.left.insert(key, value);
       }
+    /* Similarly, if the new key is greater than the node's key,
+    then you do the same thing, but on the right-hand side. */
     } else {
       if (this.right == null) {
         this.right = new BinarySearchTree(key, value, this);
       } else {
         this.right.insert(key, value);
       }
+    }
+  }
+  find(key) {
+    // if the item is found at the root, then return that value;
+    if (this.key == key) {
+      return this.value;
+    }
+    /*if the item that you are looking for is less than the root,
+    then follow the left child. If there is and existing left child,
+    then recursively check its left and/or right child 
+    until you find the item.*/
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    /*if the item that you are looking for is greater than the root,
+    then follow the right child.
+    If there is an existing right child,
+    then recursivly check its left and/or right child
+    until you find the item*/
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    //you have searched the tree, and the item isn't in the tree.
+    else {
+      throw new Error('Key Not Found');
     }
   }
 }
